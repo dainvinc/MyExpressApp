ef7cb704f345fc42a9f2c19c8048c3916888babc
Vishal <vishal.koosuri1@marist.edu>
2017-08-07 18:47:31 +0000

    Added some logic to the js file

--- app.js
+++ app.js
@@ -2,14 +2,42 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(request, response) {
-  response.send("Home");
+  response.send("Hi there, Welcome to my Home Page...");
 });
 
-
-app.get("/vishal", function(req, res) {
-  res.send("Hello, Vishal...!");
+app.get("/speak/:animal", function(request, response) {
+  // console.log(request.params);
+  if(request.params.animal === "pig") {
+    response.send("The " +request.params.animal +" says 'oink'!");
+  } else if(request.params.animal === "dog") {
+    response.send("The " +request.params.animal +" says 'Woof! Woof! '");
+  } else if(request.params.animal === "cow") {
+    response.send("The " +request.params.animal +" says 'Moo!'");
+  } else {
+    response.send("The " +request.params.animal +" says something!");
+  }
 });
+
+app.get("/repeat/:something/:id", function(request, response) {
+  var count = Number(request.params.id);
+  var res = request.params.something;
+  console.log(count);
+  if(count > 0) {
+    while(count >  1) {
+      res += " "+request.params.something;
+      count--;
+    }
+    response.send(" "+res);
+  } else {
+    response.send("There's nothing to repeat");
+  }
+});
+
+app.get("*", function(request, response) {
+  response.send("Sorry, page not found... What are you doing with your life?");
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("Server has started...");
+  console.log("Server started...");
 });
